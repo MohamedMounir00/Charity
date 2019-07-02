95ddec9223cea79f6a7e43580dd0c56a220463fc
Mohammed Mounir <m.mounir@sprints.ws>
2019-07-02 13:26:29 +0200

    v23

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -95,7 +95,8 @@
 
                                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                                     @csrf
-                                                </form>                                            </div>
+                                                </form>
+                                            </div>
                                         </div>
     
                                         <!--end: Navigation -->
